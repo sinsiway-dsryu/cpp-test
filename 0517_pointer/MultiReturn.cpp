2d90fa13af56345518f745589af7ec1d0dcdb509
dsryu <dsryu@sinsiway.com>
2022-05-19 17:46:35 +0900

    0517 포인터 숙제 시작

---
+++ 0517_pointer/MultiReturn.cpp
@@ -0,0 +1,28 @@
+﻿#include <cstdio>
+
+
+void testFunc(int* a, int* b, int* c);
+void innerTestFunc(int* a, int* b, int* c);
+
+int main(int argc, char const *argv[])
+{
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    printf("parameter values : %d, %d, %d\n", a, b, c);
+    testFunc(&a, &b, &c);
+    printf("test function result : %d, %d, %d\n", a, b, c);
+    
+    return 0;
+}
+
+void testFunc(int* a, int* b, int* c) {
+    innerTestFunc(a, b, c);
+}
+
+void innerTestFunc(int* a, int* b, int* c) {
+    *a += 1;
+    *b += 1;
+    *c += 1;
+}
+
