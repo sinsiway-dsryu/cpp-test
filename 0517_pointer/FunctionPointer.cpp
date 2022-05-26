c48223306930d1178cf1ed59ebb389d0bd3086f6
dsryu <dsryu@sinsiway.com>
2022-05-26 23:21:49 +0900

    0517 포인터 과제 리뷰

--- 0517_pointer/FunctionPointer.cpp
+++ 0517_pointer/FunctionPointer.cpp
@@ -1,15 +1,26 @@
 ﻿#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#define MAX 10
 
 void swap(int* a, int* b);
 void sort(int arr[], size_t length, void (* swap)(int* a, int* b));
 
 int main(int argc, char const *argv[])
 {
-    int arr[10] = { 2, 1, 3, 4, 5, 8, 7, 8, 9, 10 };
+
+    // int arr[10] = { 2, 1, 3, 4, 5, 8, 7, 8, 9, 10 };
+    int arr[MAX];
+    srand(time(NULL));
+    for (int i=0; i < MAX; i++) {
+        arr[i] = rand()%9;
+    }
+
     size_t arr_length = sizeof arr / sizeof arr[0];
-    void (*swap_func)(int* a, int* b);
-    swap_func = swap;
-    sort(arr, arr_length, swap_func);
+    // void (*swap_func)(int* a, int* b);
+    // swap_func = swap;
+
+    sort(arr, arr_length, swap);
 
     for (int i=0; i < arr_length; i++) {
         printf("arr : %d ", arr[i]);
