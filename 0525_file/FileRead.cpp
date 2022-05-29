7083b753d9f1c0cc0cbbc795308c483557cb40e3
dsryu <dsryu@sinsiway.com>
2022-05-29 22:43:35 +0900

    0525 파일 과제 작성 중

---
+++ 0525_file/FileRead.cpp
@@ -0,0 +1,24 @@
+﻿#include <cstdio>
+#include <cstring>
+#define MAX_STR_SIZE 100
+
+int main(int argc, char const *argv[])
+{
+    char str_read[MAX_STR_SIZE];
+    FILE* fp = fopen("file1.txt", "rt");
+    if (fp == NULL) {
+        puts("파일 오픈 실패");
+        return -1;
+    }
+
+    while (feof(fp) == 0) {
+        // memset(str_read,0,MAX_STR_SIZE); 
+        // fgets가 /n까지 넣어주고, printf의 "%s"도 그렇기에 괜찮다?
+        if (fgets(str_read, MAX_STR_SIZE, fp) != NULL) {
+            printf("%s", str_read);
+        };
+    }
+
+    fclose(fp);
+    return 0;
+}
---
+++ 0525_file/FileWrite.cpp
@@ -0,0 +1,24 @@
+﻿#include <cstdio>
+#include <cstring>
+#define MAX_STR_SIZE 100
+
+int main(int argc, char const *argv[])
+{
+    char name[MAX_STR_SIZE];
+    char account[MAX_STR_SIZE];
+    char balance[MAX_STR_SIZE];
+    FILE* fp = fopen("file2.txt", "at");
+    if (fp == NULL) {
+        puts("파일 오픈 실패");
+        return -1;
+    }
+    
+    scanf("이름: %s", name);    
+    scanf("계좌: %s", account);
+    scanf("잔액: %s", balance);
+    
+    fputs(name, fp);
+    fclose(fp);
+    return 0;
+}
+
