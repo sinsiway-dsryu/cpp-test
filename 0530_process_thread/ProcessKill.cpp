31b45d8d19a529b18bb325eb43e3d6f77ac362d6
dsryu <dsryu@sinsiway.com>
2022-05-31 17:18:35 +0900

    0530_process 과제 작업 중

--- 0530_process_thread/ProcessKill.cpp
+++ 0530_process_thread/ProcessKill.cpp
@@ -1,18 +1,45 @@
 #include <cstdio>
-#include <cstdlib>
 #include <unistd.h>
+
+#if 0
+int main(int argc, char const *argv[])
+{
+    int pid = 0;
+    int* pv1 = (int*)malloc(sizeof(int));
+
+    *pv1 = 10;
+    printf("before - pid : %d, pv1 : %d, p: %p\n", getpid(), *pv1, pv1);
+    pid = fork();
+    if(pid == 0) {
+        *pv1 = 11;
+        printf("11 - pid : %d, pv1 : %d, p: %p\n", getpid(), *pv1, pv1);
+    } else if (pid !=0) {
+        sleep(3);
+        printf("10 - pid : %d, pv1 : %d, p: %p\n", getpid(), *pv1, pv1);
+    }
+    printf("pid : %d, pv1 : %d, p: %p\n", getpid(), *pv1, pv1);
+    free(pv1);
+
+    return 0;
+}
+#else
+#include <cstdlib>
+#include <csignal>
+#define PROCESS_NUM 3
 
 int main(int argc, char const *argv[])
 {
-    int pid01;
-    int pid02;
-    int pid03;
+    FILE* fp = fopen("process.txt", "w+");
+    if (fp == NULL) {
+        puts("파일 오픈 실패");
+        return -1;
+    }
 
-    pid01 = fork();
-    pid02 = fork();
-    pid03 = fork();
-    pause();
-
-    printf("프로세스 끝 %d\n", getpid());
+    for(int i=0; i < PROCESS_NUM; i++) {
+        int pid = fork();
+    }
+    child(0, 2, fp);
+    fclose(fp);
     return 0;
 }
+#endif
