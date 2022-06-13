3ef9bc0f77df76a83a0fe16507884ce788cf90b9
dsryu <dsryu@sinsiway.com>
2022-06-13 17:14:13 +0900

    0530_thread .. 시작

--- 0530_process_thread/server_client/HelloServer.cpp
+++ 0530_process_thread/server_client/HelloServer.cpp
@@ -1,13 +1,16 @@
 ﻿#include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <signal.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #define BUF_SIZE 1024
 #define MAX_CLIENT 5
 
+void readChildProc(int sig);
 int initServer(int port);
 int acceptClient(int server_sock);
 void messageEcho(int sock);
@@ -22,36 +25,47 @@ int main(int argc, char const *argv[])
     }
     // server init
     int server_sock = initServer(atoi(argv[1]));
-    // accept and message echo
-    int pid[MAX_CLIENT];
-    int status[MAX_CLIENT];
-    memset(pid, 0, sizeof(pid));
-    memset(status, 0, sizeof(status));
 
+    // accept and message echo
     for(int i=0; i < MAX_CLIENT; i++) {
         int client_sock = acceptClient(server_sock);
-        pid[i] = fork();
-        if (pid[i] < 0) {
+        pid_t pid = fork();
+        if (pid < 0) {
             errorHandler("process fork() failed");
         }
-        if (pid[i] == 0) {
+        if (pid == 0) {
             printf("connected client %d\n", i+1);
             messageEcho(client_sock);
             close(client_sock);
             i = (i == 0) ? 0 : (i - 1);
             exit(0);
         }
-        if (pid[i] > 0) {
-            printf("child pid : %d\n", pid[i]);
-            // if (waitpid(pid[i], &status[i], WNOHANG) == -1) {
-            //     errorHandler("process wait failed");
-            // }
+        if (pid > 0) {
+            printf("child pid : %d\n", pid);
+            struct sigaction act;
+            act.sa_handler = readChildProc;
+            sigemptyset(&act.sa_mask);
+            act.sa_flags = 0;
+            sigaction(SIGCHLD, &act, 0);
         }
     }
+
     // server socket close
     close(server_sock);
 
     return 0;
+}
+
+void readChildProc(int sig) {
+    int status;
+    pid_t pid = waitpid(-1, &status, WNOHANG);
+    if (pid == -1) {
+        errorHandler("process wait failed");
+    }
+    if (WIFEXITED(status)) {
+        printf("removed proc id : %d\n", pid);
+        printf("child proc send : %d\n", WEXITSTATUS(status));
+    }
 }
 
 int initServer(int port) {
@@ -92,8 +106,8 @@ void messageEcho(int sock) {
     char read_buffer[BUF_SIZE];
     int read_length;
     while ((read_length = read(sock, read_buffer, BUF_SIZE)) != 0) {
-        printf("pid : %d / read_buffer : %s\n", getpid(), read_buffer);
         write(sock, read_buffer, read_length);
+        printf("pid : %d / read_length : %d\n", getpid(), read_length);
     }
 }
 
