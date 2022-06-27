12a9e4c66d22c9679611f394276e459881263731
dsryu <dsryu@sinsiway.com>
2022-06-27 21:25:10 +0900

    linked list 모르겠다.

---
+++ 0621_data_structure/LinkedList.cpp
@@ -0,0 +1,44 @@
+﻿#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#define WORD_MAX_SIZE 16
+
+struct List {
+    Node* head;
+    Node* current;
+};
+
+struct Node {
+    Node* next;
+    char* data;
+};
+
+List* initList(char* data) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    memset(node, 0, sizeof(Node));
+    node->next = node;
+    node->data = data;
+
+    List* list = (List*)malloc(sizeof(List));
+    memset(list, 0, sizeof(List));
+    list->head = node;
+    list->current = node;
+
+    return list;
+}
+
+int main(int argc, char const *argv[])
+{
+    FILE* fp = fopen("keywords.txt", "rt");
+    if (fp == NULL) {
+        fputs("file open error", stderr);
+        exit(1);
+    }
+
+
+
+    fscanf(fp, "%s", )
+
+
+    return 0;
+}
